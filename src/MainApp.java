a11726c92d6fdf98178f3b1f3cd7e832e9c2f167
Burcu Uğur <burcuturan737@gmail.com>
2022-10-11 20:31:59 +0300

    Dünya değişti.

---
+++ src/MainApp.java
@@ -0,0 +1,18 @@
+/**
+ * Başlangıç sınıfıdır.
+ * @link
+ *  Bu kodlara dökümantasyon kodu denir.
+ */
+public class MainApp {
+    public static void main(String[] args) {
+        /*
+         *
+         */
+    /*bir java programının başlangıç noktasında bulunmak zorunda olan metoddur. */
+        //Ekrana mesaj yazar
+        System.out.println("Merhaba..");
+        System.out.println("Dünya");
+
+
+    }
+}
